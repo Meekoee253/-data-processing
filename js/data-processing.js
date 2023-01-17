365cac8b047073eaf456d0f0b8f6f87c946e5133
Mico <m.enriquez4060@hotmail.com>
2023-01-17 09:45:29 -0800

    pt 1

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,5 +1,6 @@
 //alert("hello from data-processing");
 
+
 const queryString = window.location.search;
 console.log(queryString);
 
@@ -8,11 +9,17 @@ if(queryString.length > 0){
     const urlParams = new URLSearchParams(queryString);
 
     urlParams.forEach(function(value, key) {
-        //swaps underscore for space
-        key = key.split("_").join(" ");
 
-        myData += `<p>${key}: ${value}</p>`;
-      //console.log(value, key);
+      if(key == "Cart"){//cart
+        alert("Cart Item: " + value);
+      } else{//shipping label
+       //swaps underscore for space
+       key = key.split("_").join(" ");
+
+       myData += `<p>${key}: ${value}</p>`;
+      }
+      
+      //console.log(key, value);
     });
 
     myData += '<p><a href="index.html">CLEAR</a></p>';
